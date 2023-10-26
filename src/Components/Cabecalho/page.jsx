5155dff4d4d545f5fe7531202c9eb7050a3175ad
gabsmelin <rm550202@fiap.com.br>
2023-10-26 20:02:42 -0300

    Arrumando ambiente do projeto, para começar a trabalhar.

---
+++ src/Components/Cabecalho/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image"
+import Link from "next/link"
+import './cabecalho.scss'
+
+export default function Cabecalho() {
+    return (
+        <div className="container-cab">
+            <p><Link href="/">Home</Link></p>
+            <p><Link href="/tabela/queijo">Tabela de queijos</Link></p>
+        </div>
+    )
+  }
