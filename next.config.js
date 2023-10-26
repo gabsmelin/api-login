1fb4b411249a3964bfc69b9c08c62324b7a27c70
gabsmelin <rm550202@fiap.com.br>
2023-10-26 20:38:51 -0300

    base criada e hsting do github adicionada ao nextConfig

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "avatars.githubusercontent.com",
+                port: "",
+                pathname: "/u/**",
+            },
+        ],
+    },
+};
