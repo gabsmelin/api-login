b9cb35bb6c812814fe076e38ee270ca0e80d42c3
gabsmelin <rm550202@fiap.com.br>
2023-10-26 20:57:39 -0300

    Base - acessando arquivo json local

--- next.config.js
+++ next.config.js
@@ -3,13 +3,19 @@ const nextConfig = {}
 
 module.exports = {
     images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "avatars.githubusercontent.com",
-                port: "",
-                pathname: "/u/**",
-            },
-        ],
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "httpstatusdogs.com",
+          port: "",
+          pathname: "/img/**",
+        },
+        {
+          protocol: "https",
+          hostname: "avatars.githubusercontent.com",
+          port: "",
+          pathname: "/u/**",
+        },
+      ],
     },
-};
+  };
